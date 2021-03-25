85458f6af4068f6d7247fc9e53853678a2462e88
Yearnsmith <d.yearnsmith@gmail.com>
2021-03-25 18:38:37 +1000

    fixed formatting errors noticed while updating README.md

--- app/controllers/listings/trades_controller.rb
+++ app/controllers/listings/trades_controller.rb
@@ -3,23 +3,14 @@
 class Listings::TradesController < TradesController
   
   before_action :set_listing, only: %i[create edit update]
-  # authorize_resource :listing
 
 
   private
 
     def set_listing
       @current_listing = Listing.find(params[:listing_id])
-      # @trade_params = trade_params[:listing_attributes]
       puts "#{@current_listing.title} has been passed through. Hurray!"
       puts "trade_params: #{trade_params[:listing_attributes]}"
     end
-
-    # def trade_params
-    #   params.require(:trade).permit( :offer_id, :id, listing_attributes: [ :title, :description, :suggestion ] ) 
-    # end
-    # def id_params
-    #   params.permit(:id)
-    # end
 
 end
