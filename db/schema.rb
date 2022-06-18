dfbe217cad000090cb40a3f428e78cc8a05931fa
Derick Yearnsmith <d.yearnsmith@gmail.com>
2022-06-19 08:21:48 +1000

    update to latest rails patch to fix mimemagic issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_044655) do
+ActiveRecord::Schema.define(version: 2022_06_17_154525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
